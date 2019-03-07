ee7d60bab1bd218e6484f5bf3ef45abf91e05db4
Ken Jen Lee <kj24lee@uwaterloo.ca>
2019-03-06 19:49:35 -0500

    added task

--- Firmware/hummingbot_firmware/Hummingbot_firmware_FreeRTOS/source/Hummingbot_firmware_FreeRTOS.c
+++ Firmware/hummingbot_firmware/Hummingbot_firmware_FreeRTOS/source/Hummingbot_firmware_FreeRTOS.c
@@ -40,9 +40,27 @@
 #include "MKE14F16.h"
 #include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
-
+#include "FreeRTOS.h"
+#include "task.h"
 /* TODO: insert other definitions and declarations here. */
+#define my_task_PRIORITY (configMAX_PRIORITIES - 1)
 
+static void my_task(void *pvParameters);
+
+/*!
+
+ * @brief Task responsible for printing of "Hello world." message.
+
+ */
+
+static void my_task(void *pvParameters)
+{
+    for (;;)
+    {
+        PRINTF("Hello World! In new task!\r\n");
+        vTaskSuspend(NULL);
+    }
+}
 /*
  * @brief   Application entry point.
  */
@@ -55,6 +73,13 @@ int main(void) {
 	BOARD_InitDebugConsole();
 
     PRINTF("Hello World\n");
+    if (xTaskCreate(my_task, "my_task", configMINIMAL_STACK_SIZE + 10, NULL, my_task_PRIORITY, NULL) != pdPASS)
+	{
+		PRINTF("Task creation failed!.\r\n");
+		while (1);
+	}
+
+	vTaskStartScheduler();
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
